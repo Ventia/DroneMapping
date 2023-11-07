59b249cd2204a2e399572de13d8bff3bfc85830a
Ventia <95006465+Ventia@users.noreply.github.com>
2023-11-07 12:58:39 +0800

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,29 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+        var lyr_SatelliteMap_1 = new ol.layer.Tile({
+            'title': 'Satellite Map',
+            'type': 'base',
+            'opacity': 0.700000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}'
+            })
+        });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_SatelliteMap_1.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_SatelliteMap_1];
